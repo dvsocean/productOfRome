20e1d7468851b227d8d3042d67ca952828e03778
= <dvsocean@icloud.com>
2019-04-21 15:54:49 -0700

    started layout

---
+++ src/main/java/com/productOfRome/ProductOfRomeApplication.java
@@ -0,0 +1,13 @@
+package com.productOfRome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOfRomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOfRomeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/productOfRome/controllers/LaunchController.java
@@ -0,0 +1,13 @@
+package com.productOfRome.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LaunchController {
+
+    @RequestMapping("/")
+    public String getWelcome(){
+        return "welcome";
+    }
+}
