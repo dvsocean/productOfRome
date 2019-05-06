569b42e73a09a295fd1fcea7a13ed4e2e12a9a69
= <dvsocean@icloud.com>
2019-05-05 17:57:35 -0700

    dsfgdg

--- src/main/java/com/productOfRome/controllers/ViewController.java
+++ src/main/java/com/productOfRome/controllers/ViewController.java
@@ -14,7 +14,7 @@ public class ViewController {
 
     @RequestMapping("/")
     public String getWelcome(){
-        return "welcome";
+        return "index";
     }
 
     @RequestMapping("/selection")
